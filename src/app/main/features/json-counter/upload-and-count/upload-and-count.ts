0fea9eb7d0dd1312318b53bc4efaa685cf93cbd3
mrlancecgm <malaluan.lancechester@gmail.com>
2025-12-11 22:33:35 +0800

    PASTE AND UPLOAD COUNTER

--- src/app/main/features/json-counter/upload-and-count/upload-and-count.ts
+++ src/app/main/features/json-counter/upload-and-count/upload-and-count.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-upload-and-count',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './upload-and-count.html',
   styleUrl: './upload-and-count.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class UploadAndCount {
+  public uploadAndCountLogo: string = 'assets/images/logos/upload_and_count.png';
+  public totalCount: number = 0;
+  public hasError: boolean = false;
+  public docStyle = document.documentElement.style;
 
+  ngAfterViewInit() {
+    this.setStyleAttributes();
+  }
+  
+  setStyleAttributes() {
+    setTimeout(() => {
+      const textReference = document.getElementById('text-reference');
+      if(textReference) {
+        const referenceH = textReference.getBoundingClientRect().height;
+        this.docStyle.setProperty('--uploadAndCountLogoH',`${referenceH}px`);
+      }
+    })
+  }
 }
